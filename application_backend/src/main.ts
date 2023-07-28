20f1adb864d9c27705b744fd76f4dc8eefa4631e
alisson.coura <alisson.coura@sonda.com>
2023-07-27 22:59:26 -0300

    Cors

--- application_backend/src/main.ts
+++ application_backend/src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const corsOptions: CorsOptions = {
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Adicione os métodos que seu servidor suporta
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+  app.enableCors(corsOptions);
   await app.listen(3001);
 }
 bootstrap();
